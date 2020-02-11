e3be4c49dbff172fe199ac1e93e3be6499339e7f
William Butler <wbutler.1599@gmail.com>
2020-02-11 11:54:24 +0000

    Fixed project file errors and set up Customer Controller

---
+++ Middleware/api/Controllers/CustomersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using api.Models;
+
+namespace api.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private PRCO304_wbutlerEntities db = new PRCO304_wbutlerEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(db.CUSTOMERS.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CUSTOMER cUSTOMER = db.CUSTOMERS.Find(id);
+            if (cUSTOMER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cUSTOMER);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "customer_id,username,email_address,customer_hashed_password,password_salt,first_name,last_name,date_of_birth,address_line_one,address_line_two,postcode")] CUSTOMER cUSTOMER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CUSTOMERS.Add(cUSTOMER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cUSTOMER);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CUSTOMER cUSTOMER = db.CUSTOMERS.Find(id);
+            if (cUSTOMER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cUSTOMER);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "customer_id,username,email_address,customer_hashed_password,password_salt,first_name,last_name,date_of_birth,address_line_one,address_line_two,postcode")] CUSTOMER cUSTOMER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cUSTOMER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cUSTOMER);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CUSTOMER cUSTOMER = db.CUSTOMERS.Find(id);
+            if (cUSTOMER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cUSTOMER);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CUSTOMER cUSTOMER = db.CUSTOMERS.Find(id);
+            db.CUSTOMERS.Remove(cUSTOMER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Middleware/api/Global.asax
+++ Middleware/api/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="api.WebApiApplication" Language="VB" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="api.WebApiApplication" Language="C#" %>
