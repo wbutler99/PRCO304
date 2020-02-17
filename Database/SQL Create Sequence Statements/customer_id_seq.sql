2815f8623b409c3fb074a9dfa7fc6c7918230443
William Butler <wbutler.1599@gmail.com>
2020-02-17 15:33:12 +0000

    Added sequences for both Customer and Employee IDs

---
+++ Database/SQL Create Sequence Statements/customer_id_seq.sql
@@ -0,0 +1,4 @@
+CREATE SEQUENCE customer_id_seq
+    AS INT
+    START WITH 1
+    INCREMENT BY 1;
---
+++ Database/SQL Create Sequence Statements/employee_id_seq.sql
@@ -0,0 +1,4 @@
+CREATE SEQUENCE employee_id_seq
+    AS INT
+    START WITH 1
+    INCREMENT BY 1;
