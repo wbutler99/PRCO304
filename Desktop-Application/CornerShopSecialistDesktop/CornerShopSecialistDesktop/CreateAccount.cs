32abf7b4cc5af965494d228d6aa920ea5753a361
William Butler <wbutler.1599@gmail.com>
2020-02-20 11:20:38 +0000

    added page design for log in and account creation

--- Desktop-Application/CornerShopSecialistDesktop/CornerShopSecialistDesktop/CreateAccount.cs
+++ Desktop-Application/CornerShopSecialistDesktop/CornerShopSecialistDesktop/CreateAccount.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace CornerShopSecialistDesktop
 {
-    public partial class Form1 : Form
+    public partial class CreateAccount : Form
     {
-        public Form1()
+        public CreateAccount()
         {
             InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Desktop-Application/CornerShopSecialistDesktop/CornerShopSecialistDesktop/Home.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CornerShopSecialistDesktop
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void lblUsername_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreateUser_Click(object sender, EventArgs e)
+        {
+            CreateAccount createAccount = new CreateAccount();
+            createAccount.Show();
+        }
+    }
+}
