fc903dd7dffa7d319844359a692c22f891acdd76
William Butler <wbutler.1599@gmail.com>
2020-02-27 11:02:03 +0000

    Changed login system to accomodate staff portal

---
+++ Website/Client/Javascript/StaffLogin.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    $("#errorBox").css("visibility", "hidden");
+    $("#logInForm").submit(function(web){
+        web.preventDefault();
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        var loginDetails = {
+            "username" : username,
+            "password" : password
+        };
+        var post = $.post("http://localhost:44308/api/Staff/login", {loginDetails}).always(function(){});
+        alert("Logging you in...");
+        post.done(function(data, text, res){
+            //alert(res.status);
+            if(res.status == 200)
+            {
+                alert("Welcome " + data.username + "!");
+                $.get("http://localhost:9000/StaffAuth", {username});
+                
+                
+            }
+            else
+            {
+                alert("Log in Failed. Please check your credentials and try again.");
+                //$("#errorBox").css("visibility", "visible");
+            }
+        });
+    });
+}
