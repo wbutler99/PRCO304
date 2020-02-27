be35e60f8c419fa4c3a9c0fe61977d0be3795852
William Butler <wbutler.1599@gmail.com>
2020-02-27 10:11:52 +0000

    Changed file paths for server to allow routing to work

--- Website/corner-shop-server.js
+++ Website/corner-shop-server.js
@@ -1,11 +1,9 @@
 var express = require("express");
-var mongoose = require("mongoose");
-var db = require("./db");
-var schemas = require("./schemas");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
 var http = require("http");
+var path = require("path");
 var router = express.Router();
 
 var app = express();
@@ -18,10 +16,9 @@ app.use(function(req, res, next){
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(__dirname + '/Client/HTML'));
 
-router.get('/', function(request, response){
-    response.sendFile("/Index.html");
+app.get('/', function(request, response){
+    response.sendFile(path.join(__dirname + "/Client/HTML/Index.html"));
 });
 
 app.listen(9000, function() {
