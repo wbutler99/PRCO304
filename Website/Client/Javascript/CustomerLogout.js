907dd0f91547f1721a2b10108558de04e7feb737
William Butler <wbutler.1599@gmail.com>
2020-05-06 15:01:25 +0100

    Updated web pages to have logout script embedded to fix issue of log outs not working

--- Website/Client/Javascript/CustomerLogout.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    $("#logout").click(function(){
-        $.get("http://localhost:9000/Customer/Logout").then(function(data){
-            alert(data);
-            window.location.href = "Index.html";
-        });
-    });
-}
